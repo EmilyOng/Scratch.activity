aa21f1b5145aa5cb5196aa0857155db89566a3cb
Emily Ong <ong.huiqi.emily@dhs.sg>
2017-12-07 19:34:43 +0800

    Add files via upload

---
+++ js/activity.js
@@ -0,0 +1,9 @@
+define(["sugar-web/activity/activity"], function (activity) {
+
+	// Manipulate the DOM only when it is ready.
+	require(['domReady!'], function (doc) {
+
+		// Initialize the activity.
+		activity.setup();
+	});
+});
